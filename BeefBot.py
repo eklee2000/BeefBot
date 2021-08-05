e5f45b7acef05e37951e0c44abd6eb50ec01b25f
Ethan <ethanlee71400@gmail.com>
2021-08-05 18:14:34 -0400

    csv now outputs to data branch - can pull data branch to get csv

--- BeefBot.py
+++ BeefBot.py
@@ -18,7 +18,12 @@ client = commands.Bot(command_prefix = '!')
 github = Github(os.getenv("GITHUB_TOKEN"))
 repo = github.get_user().get_repo('beef-bot-discord')
 #path in repo
-filename = '/messagesLog.csv'
+x = repo.get_git_refs()
+for y in x:
+    print(y)
+dataRef = repo.get_git_ref("heads/data")
+
+filename = 'messagesLog.csv'
 
 @client.event
 async def on_ready():
@@ -56,20 +61,34 @@ async def Aidan(ctx): #context = ctx
 async def msgAnal(ctx):
     df = pd.DataFrame([[0]], index=[0], columns=['Filler'])
     massages = await ctx.channel.history(limit= 1000).flatten()
+
+    #Save contents to get sha every time analysis is done: new file every time!
+    csvFile = repo.get_contents(filename, ref="heads/data")
+
+    msgDict = {}
     for msg in massages:
         if msg.author != client.user:
-            if msg.author in df.columns:
-                df.at[0,msg.author] += 1
+            if msg.author.name in msgDict.keys():
+                msgDict.update({msg.author.name: msgDict[msg.author.name] + 1})
             else:
-                df[msg.author] = [1]
+                msgDict[msg.author.name] = 1
     channelName = str(ctx.channel)
-    file_location = (channelName + "data.csv")
-    #creates csv file in github?
-    await ctx.send('Anal in progress')
-    repo.create_file(filename, "PyGithub - messages data csv", df.to_csv(filename))
-    df.to_csv(file_location)
+
+    stringFormat = ''
+    arr = []
+    for x, y in msgDict.items():
+        arr.append([x,y])
+    for z in range(2):
+        for i in arr:
+            stringFormat += str(i[z])
+            stringFormat += ','
+        stringFormat = stringFormat[:-1]
+        stringFormat += '\n'
+
+    # await ctx.send('Anal in progress')
+    #creates csv file in github in data branch (not main!)
+    repo.update_file(filename, "PyGithub - messages data csv", stringFormat, csvFile.sha, branch = "data")
     print('Finished',ctx.channel)
-    await ctx.send('Anal Finished :)')    
+    # await ctx.send('Anal Finished :)')    
 
 client.run(os.getenv('BOT_TOKEN'))
-
