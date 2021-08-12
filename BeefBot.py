6673c4d6aa2c08597c844f1c927d8dd6967de2ba
Ethan Lee <47604796+eklee2000@users.noreply.github.com>
2021-08-11 23:42:23 -0400

    add substring function

--- BeefBot.py
+++ BeefBot.py
@@ -299,6 +299,14 @@ async def _stardewBaseGifts(ctx, npc: str):
         embed.add_field(name = name, value = link, inline = True)
     await ctx.send(embed = embed)
 
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
+
 def stardewTableOffset(npc):
     npcDict = {
         'Pierre': 2,
